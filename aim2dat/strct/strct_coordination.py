b921b9b3ae48a1a50a269f327d80c1093b3d490d
hdsassnick <78720500+hdsassnick@users.noreply.github.com>
2024-03-11 20:33:38 +0100

    Refactor coordination analysis module
    * Move creation of dicts containing the coordination analysis to centralized methods to make the source code more compact
    ---------
    
    Co-authored-by: hdsassnick <holger-dietrich.sassnick@uni-oldenburg.de>
    Co-authored-by: Timo Reents <77727843+t-reents@users.noreply.github.com>

--- aim2dat/strct/strct_coordination.py
+++ aim2dat/strct/strct_coordination.py
@@ -16,22 +16,6 @@ from aim2dat.utils.maths import calc_solid_angle, calc_polygon_area
 _supported_methods = ["minimum_distance", "n_nearest_neighbours", "econ", "voronoi"]
 
 
-def _coord_group_method_args(
-    min_dist_delta,
-    n_nearest_neighbours,
-    econ_tolerance,
-    econ_conv_threshold,
-    voronoi_weight_type,
-    voronoi_weight_threshold,
-):
-    return {
-        "minimum_distance": [min_dist_delta],
-        "n_nearest_neighbours": [n_nearest_neighbours],
-        "econ": [econ_tolerance, econ_conv_threshold],
-        "voronoi": [voronoi_weight_type, voronoi_weight_threshold],
-    }
-
-
 def calculate_coordination(
     structure,
     r_max: float,
@@ -47,13 +31,6 @@ def calculate_coordination(
     """
     Calculate the coordination of all sites.
     """
-
-    def calc_stdev(values):
-        if len(values) == 1:
-            return 0.0
-        else:
-            return stdev(values)
-
     if method == "okeeffe":
         from warnings import warn
 
@@ -83,9 +60,6 @@ def calculate_coordination(
         voronoi_weight_type,
         voronoi_weight_threshold,
     )
-
-    method_function = globals()["_coord_calculate_" + method]
-    coord_numbers = []
     if method == "voronoi":
         voronoi_list = structure.calculate_voronoi_tessellation(r_max=r_max)
         method_args = [structure, voronoi_list] + method_args[method]
@@ -104,44 +78,96 @@ def calculate_coordination(
             dist_matrix,
         ] + method_args[method]
 
-    for el_idx in range(len(structure["elements"])):
-        coord_numbers.append(method_function(el_idx, *method_args))
-    numbers = {}
-    distances = {}
-    weights = {}
-    for el1, coord0 in zip(structure["elements"], coord_numbers):
+    method_function = globals()["_coord_calculate_" + method]
+    sites = []
+    for site_idx in range(len(structure["elements"])):
+        sites.append(method_function(site_idx, *method_args))
+    stat_keys = ["distance"]
+    is_optional = [False]
+    if method == "voronoi" and voronoi_weight_type is not None or method == "econ":
+        stat_keys.append("weight")
+        is_optional.append(False)
+    coordination = _calculate_statistical_quantities(structure, sites, stat_keys, is_optional)
+    coordination["sites"] = sites
+    return None, coordination
+
+
+def _coord_group_method_args(
+    min_dist_delta,
+    n_nearest_neighbours,
+    econ_tolerance,
+    econ_conv_threshold,
+    voronoi_weight_type,
+    voronoi_weight_threshold,
+):
+    return {
+        "minimum_distance": [min_dist_delta],
+        "n_nearest_neighbours": [n_nearest_neighbours],
+        "econ": [econ_tolerance, econ_conv_threshold],
+        "voronoi": [voronoi_weight_type, voronoi_weight_threshold],
+    }
+
+
+def _create_site_dict(structure, site_idx, neighbours, weights=None):
+    """
+    Create dictionary storing information for a specific site.
+    """
+    site_dict = {el0: 0 for el0 in structure._element_dict.keys()}
+    site_dict["element"] = structure.elements[site_idx]
+    site_dict["kind"] = None if structure.kinds is None else structure.kinds[site_idx]
+    site_dict["position"] = list(structure.positions[site_idx])
+    site_dict["neighbours"] = []
+
+    distances = []
+    for idx, (neigh_idx, neigh_pos, distance) in enumerate(neighbours):
+        neigh_dict = {
+            "element": structure.elements[neigh_idx],
+            "kind": None if structure.kinds is None else structure.kinds[neigh_idx],
+            "site_index": neigh_idx,
+            "distance": float(distance),
+            "position": [float(pos) for pos in neigh_pos],
+        }
+        if weights:
+            neigh_dict["weight"] = float(weights[idx])
+        site_dict["neighbours"].append(neigh_dict)
+        site_dict[neigh_dict["element"]] += 1
+        distances.append(float(distance))
+
+    site_dict["total_cn"] = len(distances)
+    site_dict["min_dist"] = min(distances, default=0.0)
+    site_dict["max_dist"] = max(distances, default=0.0)
+    site_dict["avg_dist"] = sum(distances) / len(distances) if len(distances) > 0 else 0.0
+    return site_dict
+
+
+def _calculate_statistical_quantities(structure, sites, stat_keys, is_optional):
+    """
+    Calculate statistical quantities of all sites such as average/minimum/maximum etc.
+    """
+
+    def calc_stdev(values):
+        if len(values) == 1:
+            return 0.0
+        return stdev(values)
+
+    temp_lists = {k0: {} for k0 in stat_keys + ["nrs"]}
+    for el1, coord0 in zip(structure["elements"], sites):
         for el2 in structure._element_dict.keys():
             if el2 in coord0:
-                if (el1, el2) in numbers:
-                    numbers[(el1, el2)].append(coord0[el2])
-                else:
-                    numbers[(el1, el2)] = [coord0[el2]]
+                temp_lists["nrs"].setdefault((el1, el2), []).append(coord0[el2])
         for neighbour in coord0["neighbours"]:
-            if (el1, neighbour["element"]) in distances:
-                distances[(el1, neighbour["element"])].append(neighbour["distance"])
-                weights[(el1, neighbour["element"])].append(neighbour.get("weight", 0.0))
-            else:
-                distances[(el1, neighbour["element"])] = [neighbour["distance"]]
-                weights[(el1, neighbour["element"])] = [neighbour.get("weight", 0.0)]
-    coordination = {
-        "sites": coord_numbers,
-        "nrs_avg": {el_pair: mean(values) for el_pair, values in numbers.items()},
-        "nrs_stdev": {el_pair: calc_stdev(values) for el_pair, values in numbers.items()},
-        "nrs_max": {el_pair: max(values) for el_pair, values in numbers.items()},
-        "nrs_min": {el_pair: min(values) for el_pair, values in numbers.items()},
-        "distance_avg": {el_pair: mean(values) for el_pair, values in distances.items()},
-        "distance_stdev": {el_pair: calc_stdev(values) for el_pair, values in distances.items()},
-        "distance_max": {el_pair: max(values) for el_pair, values in distances.items()},
-        "distance_min": {el_pair: min(values) for el_pair, values in distances.items()},
-    }
-    if method == "voronoi" and voronoi_weight_type is not None or method == "econ":
-        coordination["weight_avg"] = {el_pair: mean(values) for el_pair, values in weights.items()}
-        coordination["weight_stdev"] = {
-            el_pair: calc_stdev(values) for el_pair, values in weights.items()
-        }
-        coordination["weight_max"] = {el_pair: max(values) for el_pair, values in weights.items()}
-        coordination["weight_min"] = {el_pair: min(values) for el_pair, values in weights.items()}
-    return None, coordination
+            for k0, optional in zip(stat_keys, is_optional):
+                value = neighbour.get(k0, 0.0) if optional else neighbour[k0]
+                temp_lists[k0].setdefault((el1, neighbour["element"]), []).append(value)
+
+    stat_functions = [("_avg", mean), ("_stdev", calc_stdev), ("_max", max), ("_min", min)]
+    stat_dict = {}
+    for global_k in temp_lists.keys():
+        for label, func in stat_functions:
+            stat_dict[global_k + label] = {
+                el_pair: func(values) for el_pair, values in temp_lists[global_k].items()
+            }
+    return stat_dict
 
 
 def _coordination_compare_sites(
@@ -172,7 +198,7 @@ def _coordination_compare_sites(
 
 
 def _coord_calculate_minimum_distance(
-    el_idx,
+    site_idx,
     structure,
     r_max,
     elements_sc,
@@ -185,52 +211,24 @@ def _coord_calculate_minimum_distance(
     """
     Calculate coordination numbers using the minimum distance method.
     """
-    el_idx_sc = indices_sc.index(el_idx)
-    min_dist = np.amin(np.delete(dist_matrix[el_idx], el_idx_sc))
-    position = np.array(structure.positions[el_idx])
-    site_details = []
-    coord_dict = {el0: 0 for el0 in structure._element_dict.keys()}
-    coord_dict["element"] = structure["elements"][el_idx]
-    coord_dict["kind"] = None if structure["kinds"] is None else structure["kinds"][el_idx]
-    coord_dict["position"] = list(structure.positions[el_idx])
-    coord_dict["total_cn"] = 0
-    coord_dict["min_dist"] = float(min_dist)
-    coord_dict["max_dist"] = 0.0
-    coord_dict["avg_dist"] = 0.0
-    distances = []
+    el_idx_sc = indices_sc.index(site_idx)
+    min_dist = np.amin(np.delete(dist_matrix[site_idx], el_idx_sc))
+    position = np.array(structure.positions[site_idx])
+    neighbours = []
     for idx in range(len(elements_sc)):
-        distance = dist_matrix[el_idx][idx]
+        distance = dist_matrix[site_idx][idx]
         if idx == el_idx_sc or distance > r_max:
             continue
         if distance >= min_dist and distance <= min_dist * (1.0 + distance_delta):
             neigh_pos = position + (
                 np.array(positions_sc[idx]) - np.array(positions_sc[el_idx_sc])
             )
-            coord_dict[elements_sc[idx]] += 1
-            coord_dict["total_cn"] += 1
-
-            site_details.append(
-                {
-                    "element": elements_sc[idx],
-                    "kind": (
-                        None if structure["kinds"] is None else structure["kinds"][mapping[idx]]
-                    ),
-                    "site_index": mapping[idx],
-                    "distance": float(distance),
-                    "position": [float(pos) for pos in neigh_pos],
-                }
-            )
-            distances.append(float(distance))
-            if distance > coord_dict["max_dist"]:
-                coord_dict["max_dist"] = float(distance)
-    if len(distances) > 0:
-        coord_dict["avg_dist"] = sum(distances) / len(distances)
-    coord_dict["neighbours"] = site_details
-    return coord_dict
+            neighbours.append((mapping[idx], neigh_pos, distance))
+    return _create_site_dict(structure, site_idx, neighbours)
 
 
 def _coord_calculate_n_nearest_neighbours(
-    el_idx,
+    site_idx,
     structure,
     r_max,
     elements_sc,
@@ -243,47 +241,21 @@ def _coord_calculate_n_nearest_neighbours(
     """
     Calculate the coordination numbers by taking the n nearest atoms.
     """
-    el_idx_sc = indices_sc.index(el_idx)
-    position = np.array(structure.positions[el_idx])
-    site_details = []
-    coord_dict = {el0: 0 for el0 in structure._element_dict.keys()}
-    coord_dict["element"] = structure["elements"][el_idx]
-    coord_dict["kind"] = None if structure["kinds"] is None else structure["kinds"][el_idx]
-    coord_dict["position"] = list(structure.positions[el_idx])
-    coord_dict["total_cn"] = 0
-    coord_dict["min_dist"] = float(np.amin(np.delete(dist_matrix[el_idx], el_idx_sc)))
-    coord_dict["max_dist"] = 0.0
-    coord_dict["avg_dist"] = 0.0
-    distances = []
-    zipped = list(zip(dist_matrix[el_idx].tolist(), range(len(elements_sc))))
+    el_idx_sc = indices_sc.index(site_idx)
+    position = np.array(structure.positions[site_idx])
+    neighbours = []
+    zipped = list(zip(dist_matrix[site_idx].tolist(), range(len(elements_sc))))
     zipped.sort(key=lambda point: point[0])
     _, sc_indices = zip(*zipped)
 
     for idx in sc_indices[1 : n_neighbours + 1]:
-        distance = dist_matrix[el_idx][idx]
         neigh_pos = position + (np.array(positions_sc[idx]) - np.array(positions_sc[el_idx_sc]))
-        coord_dict[elements_sc[idx]] += 1
-        coord_dict["total_cn"] += 1
-        site_details.append(
-            {
-                "element": elements_sc[idx],
-                "kind": None if structure["kinds"] is None else structure["kinds"][mapping[idx]],
-                "site_index": mapping[idx],
-                "distance": float(distance),
-                "position": [float(pos) for pos in neigh_pos],
-            }
-        )
-        distances.append(float(distance))
-        if distance > coord_dict["max_dist"]:
-            coord_dict["max_dist"] = float(distance)
-    if len(distances) > 0:
-        coord_dict["avg_dist"] = sum(distances) / len(distances)
-    coord_dict["neighbours"] = site_details
-    return coord_dict
+        neighbours.append((mapping[idx], neigh_pos, dist_matrix[site_idx][idx]))
+    return _create_site_dict(structure, site_idx, neighbours)
 
 
 def _coord_calculate_econ(
-    el_idx,
+    site_idx,
     structure,
     r_max,
     elements_sc,
@@ -304,60 +276,33 @@ def _coord_calculate_econ(
         dist_avg = numerator / sum(exp_dist)
         return dist_avg
 
-    el_idx_sc = indices_sc.index(el_idx)
-    position = np.array(structure.positions[el_idx])
-    distances_wo_site = np.delete(dist_matrix[el_idx], el_idx_sc)
+    el_idx_sc = indices_sc.index(site_idx)
+    position = np.array(structure.positions[site_idx])
+    distances_wo_site = np.delete(dist_matrix[site_idx], el_idx_sc)
     min_dist = np.amin(distances_wo_site)
-    site_details = []
-    coord_dict = {el0: 0 for el0 in structure._element_dict.keys()}
-    coord_dict["element"] = structure["elements"][el_idx]
-    coord_dict["kind"] = None if structure["kinds"] is None else structure["kinds"][el_idx]
-    coord_dict["position"] = list(structure.positions[el_idx])
-    coord_dict["total_cn"] = 0
-    coord_dict["min_dist"] = float(min_dist)
-    coord_dict["max_dist"] = 0.0
-    coord_dict["avg_dist"] = 0.0
-
-    distances = []
-    dist_avg0 = coord_dict["min_dist"]
+    dist_avg0 = min_dist
     dist_avg = calc_weighted_average_bond_length(distances_wo_site, dist_avg0)
     while abs(dist_avg - dist_avg0) >= conv_threshold:
         dist_avg0 = dist_avg
         dist_avg = calc_weighted_average_bond_length(distances_wo_site, dist_avg0)
 
+    neighbours = []
+    weights = []
     for idx in range(len(elements_sc)):
-        if idx == el_idx_sc or dist_matrix[el_idx][idx] > r_max:
+        if idx == el_idx_sc or dist_matrix[site_idx][idx] > r_max:
             continue
-        weight = math.exp(1.0 - (dist_matrix[el_idx][idx] / dist_avg0) ** 6.0)
+        weight = math.exp(1.0 - (dist_matrix[site_idx][idx] / dist_avg0) ** 6.0)
         if weight >= tolerance:
-            coord_dict[elements_sc[idx]] += 1
-            coord_dict["total_cn"] += 1
             neigh_pos = position + (
                 np.array(positions_sc[idx]) - np.array(positions_sc[el_idx_sc])
             )
-            site_details.append(
-                {
-                    "element": elements_sc[idx],
-                    "kind": (
-                        None if structure["kinds"] is None else structure["kinds"][mapping[idx]]
-                    ),
-                    "site_index": mapping[idx],
-                    "distance": float(dist_matrix[el_idx][idx]),
-                    "position": [float(pos) for pos in neigh_pos],
-                    "weight": weight,
-                }
-            )
-            distances.append(float(dist_matrix[el_idx][idx]))
-            if dist_matrix[el_idx][idx] > coord_dict["max_dist"]:
-                coord_dict["max_dist"] = float(dist_matrix[el_idx][idx])
-    if len(distances) > 0:
-        coord_dict["avg_dist"] = sum(distances) / len(distances)
-    coord_dict["neighbours"] = site_details
-    return coord_dict
+            neighbours.append((mapping[idx], neigh_pos, dist_matrix[site_idx][idx]))
+            weights.append(weight)
+    return _create_site_dict(structure, site_idx, neighbours, weights)
 
 
 def _coord_calculate_voronoi(
-    el_idx,
+    site_idx,
     structure,
     voronoi_list,
     weight_type,
@@ -366,28 +311,28 @@ def _coord_calculate_voronoi(
     """
     Calculate coordination numbers using o'Keeffe method.
     """
-    neighbours = voronoi_list[el_idx]
+    neighbours = voronoi_list[site_idx]
     max_weight = 0.0
     for neigh in neighbours:
         neigh["distance"] = float(
-            np.linalg.norm(neigh["position"] - structure["positions"][el_idx])
+            np.linalg.norm(neigh["position"] - structure["positions"][site_idx])
         )
         if weight_type is None:
             pass
         elif "covalent_atomic_radius" in weight_type:
             neigh["weight"] = (
-                get_atomic_radius(structure["elements"][el_idx], radius_type="covalent")
+                get_atomic_radius(structure["elements"][site_idx], radius_type="covalent")
                 + get_atomic_radius(structure["elements"][neigh["index"]], radius_type="covalent")
                 - neigh["distance"]
             )
         elif "vdw_atomic_radius" in weight_type:
             neigh["weight"] = (
-                get_atomic_radius(structure["elements"][el_idx], radius_type="vdw")
+                get_atomic_radius(structure["elements"][site_idx], radius_type="vdw")
                 + get_atomic_radius(structure["elements"][neigh["index"]], radius_type="vdw")
                 - neigh["distance"]
             )
         elif "solid_angle" in weight_type:
-            neigh["weight"] = calc_solid_angle(structure["positions"][el_idx], neigh["vertices"])
+            neigh["weight"] = calc_solid_angle(structure["positions"][site_idx], neigh["vertices"])
         elif "area" in weight_type:
             neigh["weight"] = calc_polygon_area(neigh["vertices"])
         else:
@@ -395,19 +340,9 @@ def _coord_calculate_voronoi(
         if neigh.get("weight", -1.0) > max_weight:
             max_weight = neigh["weight"]
 
-    coord_dict = {el0: 0 for el0 in structure._element_dict.keys()}
-    coord_dict = {el0: 0 for el0 in structure._element_dict.keys()}
-    coord_dict["element"] = structure["elements"][el_idx]
-    coord_dict["kind"] = None if structure["kinds"] is None else structure["kinds"][el_idx]
-    coord_dict["position"] = list(structure.positions[el_idx])
-    coord_dict["total_cn"] = 0
-    coord_dict["min_dist"] = neighbours[0]["distance"]
-    coord_dict["max_dist"] = 0.0
-    coord_dict["avg_dist"] = 0.0
-
-    site_details = []
-    distances = []
-    for neigh in voronoi_list[el_idx]:
+    neighbours = []
+    weights = []
+    for neigh in voronoi_list[site_idx]:
         add_site = True
         if weight_type is not None:
             if "rel" in weight_type:
@@ -415,24 +350,7 @@ def _coord_calculate_voronoi(
             if neigh["weight"] <= weight_threshold:
                 add_site = False
         if add_site:
-            site = {
-                "element": structure["elements"][neigh["index"]],
-                "kind": None if structure["kinds"] is None else structure["kinds"][neigh["index"]],
-                "site_index": neigh["index"],
-                "distance": neigh["distance"],
-                "position": [float(pos) for pos in neigh["position"]],
-            }
+            neighbours.append((neigh["index"], neigh["position"], neigh["distance"]))
             if weight_type is not None:
-                site["weight"] = float(neigh["weight"])
-            site_details.append(site)
-            distances.append(neigh["distance"])
-            if coord_dict["max_dist"] < neigh["distance"]:
-                coord_dict["max_dist"] = neigh["distance"]
-            if coord_dict["min_dist"] > neigh["distance"]:
-                coord_dict["min_dist"] = neigh["distance"]
-            coord_dict[site["element"]] += 1
-            coord_dict["total_cn"] += 1
-    if len(distances) > 0:
-        coord_dict["avg_dist"] = sum(distances) / len(distances)
-    coord_dict["neighbours"] = site_details
-    return coord_dict
+                weights.append(neigh["weight"])
+    return _create_site_dict(structure, site_idx, neighbours, weights)
