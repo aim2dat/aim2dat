f18f60e56c173866017b4abf3bc52913f9b7493a
hdsassnick <holger-dietrich.sassnick@uni-oldenburg.de>
2024-03-15 14:59:29 +0100

    Added custom open function.

--- aim2dat/io/fhi_aims.py
+++ aim2dat/io/fhi_aims.py
@@ -3,23 +3,21 @@ Module of functions to read output-files of FHI-aims.
 """
 
 # Standard library imports
-import os
 import re
 
-from aim2dat.io.decorators import read_multiple
+# Internal library imports
+from aim2dat.io.utils import read_multiple, custom_open
 
 
 def _check_for_soc_files(soc, folder_path):
     no_soc_suffix = False
     if all(val is None for val in folder_path["soc"]):
         raise ValueError(
-            "Spin-orbit coupling activated but the files don't have " +
-            "the proper naming scheme."
+            "Spin-orbit coupling activated but the files don't have " + "the proper naming scheme."
         )
     if not soc and any(val == ".no_soc" for val in folder_path["soc"]):
         no_soc_suffix = True
     return no_soc_suffix
-
 
 
 @read_multiple(r".*\.out(?P<soc>\.no_soc)?$")
@@ -49,20 +47,18 @@ def read_band_structure(folder_path, soc=False):
     bands = []
     kpoints = []
     for idx in indices:
-        if (folder_path["soc"][idx] is None and no_soc_suffix) or (folder_path["soc"][idx] is not None and not no_soc_suffix):
+        if (folder_path["soc"][idx] is None and no_soc_suffix) or (
+            folder_path["soc"][idx] is not None and not no_soc_suffix
+        ):
             continue
 
-        with open(folder_path["file"][idx], "r") as bandfile:
-            for line_idx, line in enumerate(bandfile):
-                nrbands = int((len(line.split()) - 4) * 0.5)
-                band0 = []
-                occupation0 = []
-                for band_idx in range(nrbands):
-                    occupation0.append(float(line.split()[2 * band_idx + 4]))
-                    band0.append(float(line.split()[2 * band_idx + 5]))
-                kpoints.append([float(line.split()[idx]) for idx in range(1, 4)])
-                bands.append(band0)
-                occupations.append(occupation0)
+        with custom_open(folder_path["file"][idx], "r") as bandfile:
+            for line in bandfile:
+                l_split = line.split()
+                nrbands = int((len(l_split) - 4) * 0.5)
+                kpoints.append([float(l_split[idx]) for idx in range(1, 4)])
+                bands.append([float(l_split[2 * idx + 5]) for idx in range(nrbands)])
+                occupations.append([float(l_split[2 * idx + 4]) for idx in range(nrbands)])
     return {"kpoints": kpoints, "unit_y": "eV", "bands": bands, "occupations": occupations}
 
 
@@ -82,7 +78,7 @@ def read_total_density_of_states(file_name):
     """
     energy = []
     tdos = []
-    with open(file_name, "r") as tdos_file:
+    with custom_open(file_name, "r") as tdos_file:
         for line in tdos_file:
             if not line.startswith("#"):
                 energy.append(float(line.split()[0]))
@@ -90,7 +86,9 @@ def read_total_density_of_states(file_name):
     return {"energy": energy, "tdos": tdos, "unit_x": "eV"}
 
 
-@read_multiple(r".*atom_proj[a-z]*_dos_(?P<kind>[a-zA-Z]+\d+)(?P<raw>_raw)?\.dat(?P<soc>\.no_soc)?$")
+@read_multiple(
+    r".*atom_proj[a-z]*_dos_(?P<kind>[a-zA-Z]+\d+)(?P<raw>_raw)?\.dat(?P<soc>\.no_soc)?$"
+)
 def read_atom_proj_density_of_states(folder_path, soc=False, load_raw=False):
     """
     Read the atom projected density of states from FHI-aims.
@@ -119,14 +117,20 @@ def read_atom_proj_density_of_states(folder_path, soc=False, load_raw=False):
     indices.sort(key=lambda point: point[0])
     _, indices = zip(*indices)
     for idx in indices:
-        if (folder_path["raw"][idx] is None and  load_raw) or (folder_path["raw"][idx] is not None and not load_raw):
+        if (folder_path["raw"][idx] is None and load_raw) or (
+            folder_path["raw"][idx] is not None and not load_raw
+        ):
             continue
-        if (folder_path["soc"][idx] is None and no_soc_suffix) or (folder_path["soc"][idx] is not None and not no_soc_suffix):
+        if (folder_path["soc"][idx] is None and no_soc_suffix) or (
+            folder_path["soc"][idx] is not None and not no_soc_suffix
+        ):
             continue
 
         pdos0 = {"element": re.split(r"(\d+)", folder_path["kind"][idx])[0]}
         energy = []
-        with open(folder_path["file"][idx], "r") as pdos_file: #TODO change to own manager:
+        with custom_open(
+            folder_path["file"][idx], "r"
+        ) as pdos_file:  # TODO change to own manager:
             for line in pdos_file:
                 if line.split()[0] != "#" and len(line.strip()) != 0:
                     energy.append(float(line.split()[0]))
