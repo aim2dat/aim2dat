5744b7f4109d5646f598e3a0ce0b0ef492ed8e8d
hdsassnick <holger-dietrich.sassnick@uni-oldenburg.de>
2024-03-23 09:50:16 +0100

    Update io.cp2k.__init__.py.

--- aim2dat/io/cp2k/__init__.py
+++ aim2dat/io/cp2k/__init__.py
@@ -2,10 +2,12 @@
 
 from aim2dat.io.cp2k.bands_dos import read_band_structure, read_atom_proj_density_of_states
 from aim2dat.io.cp2k.restart import read_optimized_structure
+from aim2dat.io.cp2k.stdout import read_stdout
 
 __all__ = [
     "read_band_structure",
     "read_total_density_of_states",
     "read_atom_proj_density_of_states",
+    "read_stdout",
     "read_optimized_structure",
 ]
