e236c8a5a678450de8d44af6c47c70840d3877df
hdsassnick <holger.sassnick@gmx.net>
2024-06-02 13:55:22 +0200

    Added new function add_structure and depreciate add_functional_group.

--- aim2dat/strct/ext_manipulation/__init__.py
+++ aim2dat/strct/ext_manipulation/__init__.py
@@ -1,9 +1,10 @@
 """External manipulation methods."""
 
 # Internal library imports
+from aim2dat.strct.ext_manipulation.add_structure import add_structure
 from aim2dat.strct.ext_manipulation.add_functional_group import (
     add_functional_group,
 )
 
 
-__all__ = ["add_functional_group"]
+__all__ = ["add_structure", "add_functional_group"]
