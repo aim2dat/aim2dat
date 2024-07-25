25f10df391e1b66f36cc36bfb74a2df6d5624c0c
hdsassnick <holger.sassnick@gmx.net>
2024-07-25 10:40:17 +0200

    Make optimade interface more stable by skipping broken links.

--- aim2dat/ext_interfaces/optimade.py
+++ aim2dat/ext_interfaces/optimade.py
@@ -80,7 +80,10 @@ def _return_database_ids(api_version, url, timeout):
                     url=prov_attr["base_url"] + f"/v{api_version}/links",
                     timeout=timeout,
                 )
-                provider_json = response.json()
+                try:
+                    provider_json = response.json()
+                except ValueError:
+                    continue
                 for database in provider_json["data"]:
                     if (
                         database["attributes"].get("link_type")
