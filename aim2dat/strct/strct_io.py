ad5ac054f8203d83cbd672d605ec3ada6c79e25a
holger <holger.sassnick@gmx.net>
2024-12-22 14:36:25 +0100

    Fix internal structure io backend and adde tests.

--- aim2dat/strct/strct_io.py
+++ aim2dat/strct/strct_io.py
@@ -16,27 +16,26 @@ def get_structure_from_file(
 ) -> Union[dict, list]:
     """Get function to read structure file."""
     found_func = False
+    file_type = ""
     if file_format is not None:
         ff_split = file_format.split("-")
-        m_name = ff_split[0]
-        f_name = "read_structure"
+        m_names = [ff_split[0]]
         if len(ff_split) > 1:
-            f_name = f"read_{ff_split[1]}_structure"
-        func = getattr(importlib.import_module("aim2dat.io." + m_name), f_name)
-        found_func = True
+            file_type = ff_split[1]
     else:
         m_names = [x.name for x in pkgutil.iter_modules(internal_io.__path__)]
-        for m_name in m_names:
-            if m_name.startswith("base") or m_name == "utils":
-                continue
-            module = importlib.import_module("aim2dat.io." + m_name)
-            for f_name, func in getmembers(module, isfunction):
-                if getattr(func, "_is_read_structure_method", False):
-                    if re.search(func._pattern, file_path):
-                        found_func = True
-                        break
-            if found_func:
-                break
+
+    for m_name in m_names:
+        if m_name.startswith("base") or m_name == "utils":
+            continue
+        module = importlib.import_module("aim2dat.io." + m_name)
+        for f_name, func in getmembers(module, isfunction):
+            if getattr(func, "_is_read_structure_method", False):
+                if re.search(func._pattern, file_path) and file_type in f_name:
+                    found_func = True
+                    break
+        if found_func:
+            break
 
     if not found_func:
         # TODO check if we can give a better error message...
