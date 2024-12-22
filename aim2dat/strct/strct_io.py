18c534c2773cfefc4fd0827d280e1aa15e9f7feb
holger <holger.sassnick@gmx.net>
2024-12-22 15:03:06 +0100

    Fix support of string-input instead of file path.

--- aim2dat/strct/strct_io.py
+++ aim2dat/strct/strct_io.py
@@ -1,6 +1,7 @@
 """Module to handle internal functions to read and write structures."""
 
 # Standard library imports
+import os
 import pkgutil
 import importlib
 from inspect import getmembers, isfunction
@@ -11,12 +12,9 @@ import re
 import aim2dat.io as internal_io
 
 
-def get_structure_from_file(
-    file_path: str, file_format: str, kwargs: dict = {}
-) -> Union[dict, list]:
-    """Get function to read structure file."""
-    found_func = False
+def _find_io_function(file_path, file_format):
     file_type = ""
+
     if file_format is not None:
         ff_split = file_format.split("-")
         m_names = [ff_split[0]]
@@ -30,16 +28,24 @@ def get_structure_from_file(
             continue
         module = importlib.import_module("aim2dat.io." + m_name)
         for f_name, func in getmembers(module, isfunction):
-            if getattr(func, "_is_read_structure_method", False):
-                if re.search(func._pattern, file_path) and file_type in f_name:
-                    found_func = True
-                    break
-        if found_func:
-            break
+            if not getattr(func, "_is_read_structure_method", False):
+                continue
+            if file_type not in f_name:
+                continue
+            if os.path.exists(file_path) and not re.search(func._pattern, file_path):
+                continue
 
-    if not found_func:
-        # TODO check if we can give a better error message...
-        raise ValueError("File format is not supported.")
+            return func
+
+    # TODO check if we can give a better error message...
+    raise ValueError("File format is not supported.")
+
+
+def get_structure_from_file(
+    file_path: str, file_format: str, kwargs: dict = {}
+) -> Union[dict, list]:
+    """Get function to read structure file."""
+    func = _find_io_function(file_path, file_format)
     kwargs.update(func._preset_kwargs)
     output = func(file_path, **kwargs)
     if isinstance(output, dict) and "structures" in output:
