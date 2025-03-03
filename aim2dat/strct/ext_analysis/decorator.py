9abba6b496633e2788a627df38a07e00e6932101
holger <holger.sassnick@gmx.net>
2025-03-03 16:19:12 +0100

    Fix external anylsis decorator and introduce cn_kwargs.

--- aim2dat/strct/ext_analysis/decorator.py
+++ aim2dat/strct/ext_analysis/decorator.py
@@ -17,14 +17,14 @@ def external_analysis_method(func):
 
     @wraps(func)
     def perform_strct_analysis(*args, **kwargs):
+        sig_pars = inspect.signature(func).parameters
         func_args = {}
-        for idx, (name, p0) in enumerate(inspect.signature(func).parameters.items()):
-            if idx < len(args):
-                func_args[name] = args[idx]
-                if name in kwargs:
-                    raise TypeError(f"{func.__name__}() got multiple values for argument '{name}'")
-            elif name in kwargs:
-                func_args[name] = kwargs[name]
+        for idx, arg in enumerate(args):
+            func_args[list(sig_pars.keys())[idx]] = arg
+        func_args.update(kwargs)
+        for keyw, par in sig_pars.items():
+            if keyw not in func_args and par.default is not par.empty:
+                func_args[keyw] = par.default
         structure = func_args.pop("structure")
         return _check_calculated_properties(structure, func, func_args)
 
