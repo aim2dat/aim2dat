a9fced98cf8e07ae21f334d153ce274e75bf3f29
Joshua Edzards <Joshua.edzards94@gmail.com>
2025-05-26 12:50:23 +0200

    Fix parser and adjust test file

--- aim2dat/io/cp2k/restart.py
+++ aim2dat/io/cp2k/restart.py
@@ -34,9 +34,16 @@ class _CellPattern(_BasePattern):
         cell = [None, None, None]
         cell_factors = [1.0, 1.0, 1.0]
         m = matches[-1]
+        cell_ref = False
         for line in m.string[m.start() : m.end()].splitlines()[1:-1]:
             line_sp = line.split()
             label = line_sp[0].upper()
+            if "REF" in label:
+                cell_ref = True
+            if cell_ref and "END" in label:
+                cell_ref = False
+            if cell_ref:
+                continue
             if label in self._dir_mapping:
                 cell[self._dir_mapping[label]] = np.array([float(val) for val in line_sp[1:]])
             elif label == "PERIODIC":
