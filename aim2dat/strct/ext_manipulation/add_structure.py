b293f1be2f1a5bde0dbd921b068cd4a48958bf34
Joshua Edzards <Joshua.edzards94@gmail.com>
2025-08-20 10:05:20 +0200

    Change  to grid search if  fails and enhance the algorithm

--- aim2dat/strct/ext_manipulation/add_structure.py
+++ aim2dat/strct/ext_manipulation/add_structure.py
@@ -4,8 +4,9 @@ Module that implements routines to add a functional group or adsorbed molecule t
 
 # Standard library imports
 import os
-from typing import Union, List
 import copy
+from typing import Union, List
+from itertools import product, combinations
 
 # Third party library imports
 import numpy as np
@@ -22,7 +23,10 @@ from aim2dat.strct.ext_manipulation.utils import (
 from aim2dat.strct.ext_manipulation.rotate_structure import rotate_structure
 from aim2dat.strct.strct_misc import _calc_atomic_distance
 from aim2dat.elements import get_element_symbol
-from aim2dat.utils.maths import calc_angle, create_lin_ind_vector
+from aim2dat.utils.maths import (
+    calc_angle,
+    create_lin_ind_vector,
+)
 from aim2dat.io import read_yaml_file
 
 
@@ -128,8 +132,7 @@ def add_structure_random(
             range(len(new_structure) - len(guest_strct["elements"]), len(new_structure))
         )
 
-        is_added = _check_distances(new_structure, new_indices, None, distance_dict, True)
-        if is_added:
+        if _check_distances(new_structure, new_indices, None, distance_dict, True):
             return new_structure, "_added-" + guest_strct_label
     raise DistanceThresholdError(
         "Could not add guest structure, host structure seems to be too aggregated."
@@ -141,12 +144,11 @@ def add_structure_coord(
     structure: Structure,
     wrap: bool = False,
     host_indices: Union[int, List[int]] = 0,
-    guest_index: int = 0,
+    guest_indices: Union[int, List[int]] = 0,
     guest_structure: Union[Structure, str] = "CH3",
-    guest_dir: Union[None, List[float]] = None,
+    rotate_guest: bool = False,
     bond_length: float = 1.25,
-    dist_constraints=None,
-    constrain_steps: int = 1000,
+    constrain_steps: int = 90,
     dist_threshold: Union[dict, list, float, int, str, None] = 0.8,
     change_label: bool = False,
     **cn_kwargs,
@@ -163,25 +165,23 @@ def add_structure_coord(
     host_indices : int or list (optional)
         Index or indices of the host site(s). In case several indices are given the center of the
         sites is defined as host site as reference point for the bond length.
-    guest_index : int (optional)
-        Index of the guest site.
+    guest_indices : int or list (optional)
+        Index or indices of the guest site. In case several indices are given the center of the
+        sites will face the host structure. The rest of the guest will point away.
     guest_structure : str or aim2dat.strct.Structure (optional)
         A representation of the guest structure given as a string of a functional group or molecule
         (viable options are ``'CH3'``, ``'COOH'``, ``'H2O'``, ``'NH2'``, ``'NO2'`` or ``'OH'``), a
         ``Structure`` object or the element symbol to add one single atom.
-    guest_dir: list of floats (optional)
-        Defines the orientation of the guest molecule. If not defined, a vector of nearest
-        neighbors is constructed based on the guest index.
     bond_length : float
         Bond length between the host atom and the base atom of the functional group.
-    dist_constraints : list (optional)
-        List of three-fold tuples containing the index of the site of the host structure, the index
-        of the site of the guest structure and the target distance. The position of the guest
-        structure is varied based on a grid search until the sum of the absolute errors in
-        minimized.
-    constrain_steps: int (optional)
-        Number of steps to consider for the grid search in `dist_constraints`. 1000 steps consider
-        10 different rotation angles around x, y, and z.
+    rotate_guest : bool (optional)
+        The rotation of the guest structure is varied based on a grid search until the sum of the
+        absolute errors is minimized.
+    constrain_steps : int (optional)
+        Rotation steps in degree to consider for the grid search in `rot_guest`.
+        Additionally, if the first position guess of the molecule does not match the
+        dist_threshold`, a grid search around the host atom is utilized until the sum of the
+        absolute errors is minimized.
     dist_threshold : dict, list, float, int, str or None (optional)
         Check the distances between all site pairs of the host and guest structure to ensure that
         none of the added atoms collide or are too far apart. For example, ``0.8`` to ensure a
@@ -213,103 +213,136 @@ def add_structure_coord(
     ValueError
         If any distance between atoms is outside the threshold.
     """
+    # Checks
     guest_strct, guest_strct_label = _check_guest_structure(guest_structure)
+    dist_dict, _ = _build_distance_dict(dist_threshold, structure, guest_strct)
     if isinstance(host_indices, int):
         host_indices = [host_indices]
+    if isinstance(guest_indices, int):
+        guest_indices = [guest_indices]
 
-    if max(host_indices) >= len(structure) or guest_index >= len(guest_strct):
-        idx = max(host_indices)
-        return structure
+    if max(host_indices) >= len(structure) or max(guest_indices) >= len(guest_strct):
+        return structure, ""
 
-    if dist_constraints is None:
-        dist_constraints = []
-
-    if guest_dir is None:
+    if len(guest_strct) == 1:
         guest_dir = [1.0, 0.0, 0.0]
-        if len(guest_strct) > 1:
-            # Get vector of guest atoms for rotation
-            guest_strct_coord = guest_strct.calc_coordination(**cn_kwargs)
-            guest_dir = -1.0 * _derive_bond_dir(guest_index, guest_strct_coord)
+    else:
+        # Get vector of guest atoms for rotation
+        guest_dir, *_ = _derive_bond(guest_strct, guest_indices, cn_kwargs)
+        guest_dir *= -1.0
     guest_dir /= np.linalg.norm(np.array(guest_dir))
 
     # Calculate coordination:
-    coord = structure.calc_coordination(**cn_kwargs)
 
-    # Derive bond directions and rotation to align guest towards bond direction:
-    bond_dir = _derive_bond_dir(host_indices[0], coord)
-    host_positions = [structure.get_positions()[host_indices[0]]]
-    for idx in host_indices[1:]:
-        bond_dir += _derive_bond_dir(idx, coord)
-        _, pos = _calc_atomic_distance(structure, host_indices[0], idx, backfold_positions=True)
-        host_positions.append(pos)
-    bond_dir /= np.linalg.norm(bond_dir)
-    host_pos_np = np.mean(np.array(host_positions), axis=0)
-    if len(guest_strct) > 1:
-        rot_angle = np.rad2deg(-calc_angle(guest_dir, bond_dir))
-        if np.isclose(abs(rot_angle), 0.0) or np.isclose(abs(rot_angle), 180.0):
-            guest_dir = create_lin_ind_vector(guest_dir)
-        guest_strct = rotate_structure(
-            structure=guest_strct,
-            vector=np.cross(bond_dir, guest_dir),
-            angles=rot_angle,
-            origin=guest_strct["positions"][guest_index],
-        )
+    # Derive bond directions
+    bond_dir, host_pos_np, bond_positions = _derive_bond(
+        structure, host_indices, cn_kwargs, bond_length
+    )
 
     # Check bond length and adjusts if necessary
-    if all(host_pos_np == host_positions[0]):
-        pass
-    elif all(
-        [np.linalg.norm(host_pos_np - host_pos) >= bond_length for host_pos in host_positions]
-    ):
+    if all([np.linalg.norm(host_pos_np - bond_pos) >= bond_length for bond_pos in bond_positions]):
         bond_length = 0.0
     else:
-        bond_length = _rescale_bond_length(host_pos_np, host_positions, bond_dir, bond_length)
+        bond_length = _rescale_bond_length(host_pos_np, bond_positions, bond_dir, bond_length)
 
-    # # Define reference directions for rotations:
-    ref_dir_alpha = bond_dir
-    ref_dir_beta = np.cross(bond_dir, create_lin_ind_vector(bond_dir))
-    ref_dir_beta /= np.linalg.norm(ref_dir_beta)
-    ref_dir_gamma = np.cross(bond_dir, ref_dir_beta)
-    ref_dir_gamma /= np.linalg.norm(ref_dir_gamma)
-    ref_dirs = [ref_dir_alpha, ref_dir_beta, ref_dir_gamma]
+    # Rotate guest to align the x-axis
+    if len(guest_strct) > 1:
+        rot_angle = np.rad2deg(-calc_angle(guest_dir, [1, 0, 0]))
+        if np.isclose(abs(rot_angle), 180.0):
+            guest_dir = create_lin_ind_vector(guest_dir)
+        if not np.isclose(abs(rot_angle), 0.0):
+            guest_pos = np.array(guest_strct.positions)
+            origin = np.mean(guest_pos[guest_indices], axis=0)
+            guest_strct = rotate_structure(
+                structure=guest_strct,
+                vector=np.cross([1, 0, 0], guest_dir),
+                angles=rot_angle,
+                origin=origin,
+            )
 
-    # # Create new structure
-    new_structure, score = _add_mol(
+    # Create new structure
+    new_structure, new_guest = _add_mol(
         structure,
         guest_strct,
         wrap,
         host_pos_np,
         bond_length,
         [0.0, 0.0, 0.0],
-        ref_dirs,
-        dist_constraints,
+        bond_dir,
     )
-    new_indices = list(
-        range(len(new_structure) - len(guest_strct["elements"]), len(new_structure))
-    )
+    new_indices = list(range(len(new_structure) - len(guest_strct), len(new_structure)))
 
     # Optimize positions to reduce score
-    dist_dict, _ = _build_distance_dict(dist_threshold, structure, guest_strct)
-    num = round(constrain_steps ** (1 / 3))
-    if len(dist_constraints) > 0:
-        for alpha in np.linspace(0, 360, num=num):
-            for beta in np.linspace(-180, 180, num=num):
-                for gamma in np.linspace(-180, 180, num=num):
-                    new_strct0, score0 = _add_mol(
+    # Numbers for the grid search
+    num1 = round(360 / constrain_steps)
+    num2 = round(num1 / 2)
+    score = _check_distances(new_structure, new_indices, None, dist_dict, True, True)
+    # If the distance threshold does not hold, a grid search will be performed.
+    # The grid is based on `constrain_steps` and rotates the molecule around `host_indices` within
+    # a sphere with the ´bond_distance´ as rotation vector.
+    if not score:
+        score = score if score else float("inf")
+        for alpha in np.linspace(-180, 180, num=num1, endpoint=False):
+            for beta in np.linspace(0, 180, num=num2, endpoint=False):
+                try:
+                    new_strct0, new_guest0 = _add_mol(
                         structure,
                         guest_strct,
                         wrap,
                         host_pos_np,
                         bond_length,
-                        [alpha, beta, gamma],
-                        ref_dirs,
-                        dist_constraints,
+                        [0.0, alpha, beta],
+                        bond_dir,
                     )
-                    if score0 < score and _check_distances(
-                        new_strct0, new_indices, None, dist_dict, True
-                    ):
-                        score = score0
-                        new_structure = new_strct0
+                except Exception:
+                    continue
+                score0 = _check_distances(new_strct0, new_indices, None, dist_dict, True, True)
+                if score0 and score0 < score:
+                    score = score0
+                    new_structure = new_strct0
+                    new_guest = new_guest0
+
+    # A grid search will be performed when `rotate_guest` is set to `True`.
+    # The grid is based on `constrain_steps` and rotates the molecule around its own axis.
+    if rotate_guest:
+        score = score if score else float("inf")
+        # Prepare guest to rotate. Need to align guest with x-axis.
+        origin = np.mean(new_guest.positions, axis=0)
+        guest_vec = origin - host_pos_np
+        rot_angle = np.rad2deg(-calc_angle(guest_vec, [1, 0, 0]))
+        if np.isclose(abs(rot_angle), 180.0):
+            guest_vec = create_lin_ind_vector(guest_vec)
+        if not np.isclose(abs(rot_angle), 0.0):
+            new_guest = rotate_structure(
+                new_guest,
+                rot_angle,
+                np.cross([1, 0, 0], guest_vec),
+                origin,
+            )
+
+        for alpha, beta in product(np.linspace(-180, 180, num=num1, endpoint=False), repeat=2):
+            for gamma in np.linspace(0, 180, num=num2, endpoint=False):
+                new_guest0 = rotate_structure(
+                    new_guest,
+                    [alpha, beta, gamma],
+                    origin=origin,
+                )
+                try:
+                    # Need to align guest with original position.
+                    new_guest0 = rotate_structure(
+                        new_guest0,
+                        -rot_angle,
+                        np.cross([1, 0, 0], guest_vec),
+                        origin,
+                    )
+                    new_strct0 = _merge_structures(structure, new_guest0, wrap)
+                except Exception:
+                    continue
+                score0 = _check_distances(new_strct0, new_indices, None, dist_dict, True, True)
+                if score0 and score0 < score:
+                    score = score0
+                    new_structure = new_strct0
+
     _check_distances(new_structure, new_indices, None, dist_dict, False)
     return new_structure, "_added-" + guest_strct_label
 
@@ -353,7 +386,7 @@ def add_structure_position(
     Returns
     -------
     aim2dat.strct.Structure
-        Structure with added sub structure at defined postion.
+        Structure with added sub structure at defined position.
 
     Raises
     ------
@@ -417,30 +450,51 @@ def _check_guest_structure(guest_strct: Union[Structure, str]) -> Structure:
         raise TypeError("`guest_structure` needs to be of type Structure or str.")
 
 
-def _derive_bond_dir(index, coord):
-    # Derive bond direction and rotation to align guest towards bond direction:
-    cn_details = coord["sites"][index]
-    pos = np.array(cn_details["position"])
-    bond_dir = np.zeros(3)
-    for neigh in cn_details["neighbours"]:
-        dir_v = np.array(neigh["position"]) - pos
-        bond_dir += dir_v / np.linalg.norm(dir_v)
-    # We need to ensure that the bond direction is not zero.
-    # Happens when the neighbours are exatly opposite of each other or in a symmetric arrangment.
-    if np.linalg.norm(bond_dir) < 1e-1:
-        bond_dir = np.cross(
-            np.array(cn_details["neighbours"][0]["position"]) - pos,
-            np.array(cn_details["neighbours"][1]["position"]) - pos,
-        )
-    # We need to ensure that the bond direction is not zero.
-    # Can happen when chosen neigbours are pointing exactly opposite of each other from host atom.
-    if np.linalg.norm(bond_dir) < 1e-1:
-        bond_dir = np.cross(
-            create_lin_ind_vector(np.array(cn_details["neighbours"][0]["position"]) - pos),
-            np.array(cn_details["neighbours"][0]["position"]) - pos,
-        )
-    bond_dir *= -1.0 / np.linalg.norm(bond_dir)
-    return bond_dir
+def _derive_bond(structure, index, cn_kwargs, bond_length=None):
+    coord = structure.calc_coordination(**cn_kwargs)
+    bond_direction = np.zeros(3)
+    bond_positions = []
+    if isinstance(index, int):
+        index = [index]
+    for idx in index:
+        bond_dir = np.zeros(3)
+        cn_details = coord["sites"][idx]
+        pos = np.array(cn_details["position"])
+        all_pos = np.array([site["position"] for site in coord["sites"]])
+        if not bond_positions:
+            bond_positions.append(pos)
+        else:
+            _, pos = _calc_atomic_distance(structure, index[0], idx, backfold_positions=True)
+            bond_positions.append(pos)
+        for neigh in cn_details["neighbours"]:
+            dir_v = np.array(neigh["position"]) - pos
+            bond_dir += dir_v / np.linalg.norm(dir_v)
+        # We need to ensure that the bond direction is not zero.
+        if len(cn_details["neighbours"]) == 2 and np.linalg.norm(bond_dir) < 1e-1:
+            bond_dir = np.cross(
+                create_lin_ind_vector(np.array(cn_details["neighbours"][0]["position"]) - pos),
+                np.array(cn_details["neighbours"][0]["position"]) - pos,
+            )
+        # We need to ensure that the bond direction is not zero.
+        elif np.linalg.norm(bond_dir) < 1e-1:
+            max_dist = 0.0
+            for neigh1, neigh2 in combinations(cn_details["neighbours"], 2):
+                cross_dir = np.cross(
+                    np.array(neigh1["position"]) - pos,
+                    np.array(neigh2["position"]) - pos,
+                )
+                if np.linalg.norm(cross_dir) < 1e-1:
+                    continue
+                pos1 = pos + cross_dir / np.linalg.norm(cross_dir) * bond_length
+                diffs = all_pos - pos1
+                dists = np.linalg.norm(diffs, axis=1)
+                if min(dists) > max_dist:
+                    bond_dir = cross_dir
+        bond_dir *= -1.0 / np.linalg.norm(bond_dir)
+        bond_direction += bond_dir
+    bond_position_center = np.mean(np.array(bond_positions), axis=0)
+    bond_direction /= np.linalg.norm(bond_direction)
+    return bond_direction, bond_position_center, bond_positions
 
 
 def _rescale_bond_length(host_pos_np, host_positions, bond_dir, bond_length):
@@ -471,31 +525,45 @@ def _rescale_bond_length(host_pos_np, host_positions, bond_dir, bond_length):
 
 
 def _add_mol(
-    structure, guest_strct, wrap, host_pos, bond_length, angle_pars, ref_dirs, dist_constraints
+    structure,
+    guest_strct,
+    wrap,
+    host_pos,
+    bond_length,
+    angles,
+    bond_dir,
 ):
-    # Reorient and shift guest structure:
-    for p0, ref_dir in zip(angle_pars, ref_dirs):
-        guest_strct = rotate_structure(guest_strct, angles=p0, vector=ref_dir, origin=np.zeros(3))
-    guest_strct.set_positions(
-        [np.array(pos) + bond_length * ref_dirs[0] + host_pos for pos in guest_strct.positions]
+    guest_strct0 = guest_strct.copy()
+    guest_pos = np.array(guest_strct.positions) + bond_length * np.array([1, 0, 0])
+    # Shift guest structure:
+    guest_strct0.set_positions(guest_pos)
+    # Rotate if `dist_threshold` is not satisfied
+    guest_strct0 = rotate_structure(
+        guest_strct0,
+        angles,
+        origin=np.zeros(3),
     )
 
-    # Add guest structure to host:
-    new_structure = _merge_structures(structure, guest_strct, wrap)
+    # Rotate guest to align bond direction
+    if len(guest_strct0) > 1:
+        rot_angle = np.rad2deg(-calc_angle([1, 0, 0], bond_dir))
+        if np.isclose(abs(rot_angle), 0.0):
+            pass
+        elif np.isclose(abs(rot_angle), 180.0):
+            rot_dir = np.cross(create_lin_ind_vector(bond_dir), [1, 0, 0])
+        else:
+            rot_dir = np.cross(bond_dir, [1, 0, 0])
+        guest_strct0 = rotate_structure(
+            guest_strct0,
+            rot_angle,
+            rot_dir,
+            np.zeros(3),
+        )
+    guest_strct0.set_positions(guest_strct0.positions + host_pos)
 
-    # Evaluate complex based on the distance constraints:
-    score = 0.0
-    if len(dist_constraints) > 0:
-        host_indices = [idx for idx, _, _ in dist_constraints]
-        guest_indices = [idx + len(structure) for _, idx, _ in dist_constraints]
-        ref_dists = [dist for _, _, dist in dist_constraints]
-        dists = new_structure.calc_distance(host_indices, guest_indices, backfold_positions=True)
-        if isinstance(dists, float):
-            dists = [dists]
-        elif isinstance(dists, dict):
-            dists = [dists[tuple(idx)] for idx in zip(list(host_indices), list(guest_indices))]
-        score = sum(abs(dist - ref_dist) for dist, ref_dist in zip(dists, ref_dists))
-    return new_structure, score
+    # Add guest structure to host:
+    new_structure = _merge_structures(structure, guest_strct0, wrap)
+    return new_structure, guest_strct0
 
 
 def _merge_structures(host_strct, guest_strct, wrap):
