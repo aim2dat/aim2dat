fd686c0212c0b15ac969c6f9b43763d4ec31a73f
Scemp <Joshua.edzards94@gmail.com>
2026-06-03 09:56:26 +0200

    Add charge density difference as calcjob and implement in workchain.

---
+++ aim2dat/aiida_workflows/cubecruncher/calcjobs.py
@@ -0,0 +1,129 @@
+"""
+Calcjobs for the cubecruncher software package. The calculations are performed via cubecruncher.
+Please make sure to set the path `cubecruncher_executable` correctly.
+The calculation will the be submitted via `bash`.
+Make sure to have the code `bash` implemented `verdi code create core.code.installed`.
+"""
+
+# Third party library imports
+from aiida.common.datastructures import CalcInfo, CodeInfo
+from aiida.engine import CalcJob
+import aiida.orm as aiida_orm
+from aiida.plugins import DataFactory
+
+
+GCubeData = DataFactory("aim2dat.gaussian_cube")
+
+
+class CubecruncherCalculation(CalcJob):
+    """
+    Calcjob for the cubecruncher software package. The calculations are performed via cubecruncher.
+    Please make sure to set the path `cubecruncher_executable` correctly. The calculation will the
+    be submitted via `bash`.
+    Make sure to have the code `bash` implemented `verdi code create core.code.installed`.
+    """
+
+    @classmethod
+    def define(cls, spec):
+        """Define input/output and outline."""
+        super().define(spec)
+        spec.input(
+            "parameters",
+            valid_type=aiida_orm.List,
+            default=lambda: aiida_orm.List([]),
+            help=""
+            "List of input parameters for the final charge-density-difference cube calculation.",
+        )
+        spec.input(
+            "cubecruncher_executable",
+            valid_type=aiida_orm.Str,
+            default=lambda: aiida_orm.Str("cubecruncher.x"),
+            help="Path or executable name of cubecruncher.",
+        )
+        spec.input(
+            "charge_density_folder",
+            valid_type=aiida_orm.RemoteData,
+            help="Folder containing the charge-density cube files",
+        )
+        spec.inputs["metadata"]["options"]["input_filename"].default = "run_cubecruncher.sh"
+        spec.inputs["metadata"]["options"]["output_filename"].default = "cdd.cube"
+        spec.inputs["metadata"]["options"]["resources"].default = {
+            "num_machines": 1,
+            "num_mpiprocs_per_machine": 1,
+        }
+        spec.inputs["metadata"]["options"]["withmpi"].default = False
+        spec.inputs["metadata"]["options"]["parser_name"].default = "aim2dat.cubecruncher"
+        spec.output(
+            "charge_density_difference_folder",
+            valid_type=aiida_orm.RemoteData,
+            help="Folder containing the charge-density-difference cube files.",
+        )
+        spec.output(
+            "cdd_cube",
+            valid_type=GCubeData,
+            help="Charge-density-difference cube file.",
+        )
+        spec.exit_code(
+            310, "ERROR_READING_OUTPUT_FILE", message="The output file could not be read."
+        )
+        spec.exit_code(
+            401,
+            "ERROR_CHARGE_DENSITY_FILES",
+            message="Charge density files not found in remote folder.",
+        )
+
+    def prepare_for_submission(self, folder):
+        """Prepare for submission."""
+        cd_folder = self.inputs.charge_density_folder
+        remote_path = self.inputs.charge_density_folder.get_remote_path()
+        comp_uuid = self.inputs.charge_density_folder.computer.uuid
+        copy_links = []
+        cube_files = sorted([file for file in cd_folder.listdir() if file.endswith(".cube")])
+        for cube_f in cube_files:
+            copy_links.append((comp_uuid, remote_path + "/" + cube_f, cube_f))
+        cube_files = list(reversed(cube_files))
+
+        cubecruncher_exec = self.inputs.cubecruncher_executable.value
+
+        with folder.open(self.options.input_filename, "w", encoding="utf8") as handle:
+            handle.write("#!/bin/bash\n\n")
+            handle.write("set -e\n\n")
+            parameters = self.inputs.parameters.get_list()
+            parameters_str = " ".join(f"-{parameter}" for parameter in parameters)
+            for i, cube_f in enumerate(cube_files[1:], start=1):
+
+                input_cube = cube_files[0] if i == 1 else f"tmp{i-1}.cube"
+                output_cube = "cdd.cube" if cube_f == cube_files[-1] else f"tmp{i}.cube"
+
+                cmd = (
+                    f"{cubecruncher_exec} "
+                    f"-i {input_cube} "
+                    f"-o {output_cube} "
+                    f"-subtract {cube_f}"
+                )
+
+                if output_cube == "cdd.cube":
+                    cmd += f" {parameters_str}"
+
+                handle.write(cmd + "\n")
+
+            handle.write("\nrm -f tmp*.cube\n")
+
+        codeinfo = CodeInfo()
+        codeinfo.code_uuid = self.inputs.code.uuid
+        codeinfo.stdin_name = self.options.input_filename
+        codeinfo.cmdline_params = [self.options.input_filename]
+
+        calcinfo = CalcInfo()
+        calcinfo.codes_info = [codeinfo]
+        calcinfo.local_copy_list = []
+        if self.inputs.code.computer.uuid == cd_folder.computer.uuid:
+            calcinfo.remote_symlink_list = copy_links
+        else:
+            self.report(
+                f"Transferring files from {cd_folder.computer.label} to "
+                f"{self.inputs.code.computer.label}."
+            )
+            calcinfo.remote_copy_list = copy_links
+        calcinfo.retrieve_temporary_list = [self.options.output_filename]
+        return calcinfo
---
+++ aim2dat/aiida_workflows/cubecruncher/parsers.py
@@ -0,0 +1,33 @@
+"""
+Parsers for the cubecruncher software package.
+"""
+
+# Standard library imports
+import pathlib
+
+# Third party library imports
+from aiida.parsers import Parser
+from aiida.plugins import DataFactory
+
+
+GCubeData = DataFactory("aim2dat.gaussian_cube")
+
+
+class CubecruncherParser(Parser):
+    """
+    Output parser for the cubecruncher software package.
+    """
+
+    def parse(self, **kwargs):
+        """Receives in input a dictionary of retrieved nodes. Does all the logic here."""
+        retrieved_temporary_folder = kwargs.get("retrieved_temporary_folder", None)
+
+        try:
+            retrieved_temporary_folder = pathlib.Path(retrieved_temporary_folder)
+
+            for subpath in enumerate(retrieved_temporary_folder.iterdir()):
+                if subpath.is_file():
+                    g_cube_data = GCubeData.set_from_file(subpath)
+                self.out("cdd_cube", g_cube_data)
+        except OSError:
+            return self.exit_codes.ERROR_READING_OUTPUT_FILE
